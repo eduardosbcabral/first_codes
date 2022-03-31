a24bd59d959730fae2c156eed99e75534631f954
Eduardo Cabral <eduardosbcabral@gmail.com>
2022-03-31 10:18:19 -0300

    adds js codes

---
+++ js/ex5-compiladores/ex5-compiladores/app.js
@@ -0,0 +1,106 @@
+app.controller('ex5Ctrl', function ($scope) {
+    
+        var matchPattern = /^((?!\d).)*([\w]+[\s]?[\=][\s]?)([\w]+[\s]?)([\+\-\*\/][\s]?)([\w]+[\s]?)([\+\-\*\/][\s]?[\w]+)?([\;]$)/;
+        var variableRegex = /[\w\=\;\+\-\*\/]/;
+        var numberRegex = /^\d/;
+        var operatorsRegex = /\+|\-|\*|\//;
+    
+        function geraResultado(resultado, index, lastIndex) {        
+                return {
+                    'resultado': resultado,
+                    'index': index,
+                    'lastIndex': lastIndex
+                };
+            }
+    
+        $scope.verifyPattern = function () {
+            $scope.matched = matchPattern.test($scope.targetString);
+            
+            if($scope.matched){
+                angular.element(document.querySelector('#targetString')).removeClass('border border-danger');            
+                angular.element(document.querySelector('#targetString')).addClass('border border-success');
+    
+                $scope.identificadores = [];
+
+                var phrases = fixPhrases();
+                phrases.push(";");
+                var idq = 1;
+    
+                for(var i=0; i<phrases.length; i++) {
+                    if(numberRegex.test(phrases[i])){
+                        var obj = {
+                            lex: phrases[i],
+                            token: 'Número'
+                        }
+                        $scope.identificadores.push(obj);
+                    } else
+                    if(variableRegex.test(phrases[i])){
+
+                        var obj = {
+                            lex: phrases[i]
+                        };
+        				console.log(obj);
+                        if(phrases[i]=="+"){
+                            obj.token = "Operador aritmético ADD"
+                        } else
+                        if(phrases[i]=="="){
+                        	obj.token = "Operador de atribuição"
+                        } else
+                        if(phrases[i]=="-"){
+                            obj.token = "Operador aritmético SUB"
+                        } else
+                        if(phrases[i]=="*"){
+                            obj.token = "Operador aritmético MULT"
+                        } else
+                        if(phrases[i]=="/"){
+                            obj.token = "Operador aritmético DIV"
+                        }else
+                        if(phrases[i]==";"){
+                            obj.token = "Delimitador"
+                        } else {
+                            obj.token = 'Identificador ' + idq;
+                            idq++;
+                        }
+                        $scope.identificadores.push(obj);
+                    }
+                    
+                }
+    
+            } else {
+                angular.element(document.querySelector('#targetString')).removeClass('border border-success');            
+                angular.element(document.querySelector('#targetString')).addClass('border border-danger');            
+            }
+        }
+    
+        function fixPhrases() {
+            var resultados	 = [];
+            var resultado 	 = null;
+        
+            var objetoRegex = new RegExp(variableRegex, 'g');
+            var indexAtual = 0;
+        
+            while (resultado = objetoRegex.exec($scope.targetString)) {
+                console.log(resultado.index, indexAtual);
+                if(resultado.index!=indexAtual){
+                	console.log("AAAA:" ,resultado[0])
+                    resultado[0] = ';' + resultado[0];
+                }
+                indexAtual = objetoRegex.lastIndex;
+                resultados.push(geraResultado(resultado[0], resultado.index, objetoRegex.lastIndex));
+            }
+            var palavra1 = "";
+            var palavra2 = "";
+    
+            for(var i=0; i<resultados.length; i++){
+                palavra1 = palavra1 + resultados[i].resultado;
+            }
+            
+            for(var i=0;i<palavra1.length; i++) {
+                palavra2 = palavra2 + palavra1.charAt(i);
+            }
+    
+            return palavra2.split(";");
+        }
+    
+    })
+    
